f765fd57f1ddf2ddaf5ae5c582ec30a2a612edc9
Sandeep Reddy <sandeep.thippareddy.ext@zeiss.com>
2023-04-10 19:18:56 +0530

    Add project files.

---
+++ EventGridProducer/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace EventHubProducer
+{
+    internal class Order
+    {
+        public int OrderId { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int Quality { get; set; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
---
+++ EventGridProducer/Program.cs
@@ -0,0 +1,43 @@
+﻿using Azure.Messaging.EventHubs.Producer;
+using EventHubProducer;
+using System.Text;
+
+var connectionString = "Endpoint=sb://eventhubnamespace212.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=LRBgw9AzvxwBpLEGfT9F8UJWrxSSdzbWj+AEhOlBKWI=;EntityPath=demoeventhub";
+
+EventHubProducerClient producerClient = new EventHubProducerClient(connectionString);
+
+var listOfOrders = new List<Order>() {
+    new Order() {
+    OrderId= 1,
+    Quality= 76,
+    UnitPrice = 23
+    },
+       new Order() {
+    OrderId= 2,
+    Quality= 76,
+    UnitPrice = 23
+    },
+          new Order() {
+    OrderId= 3,
+    Quality= 76,
+    UnitPrice = 23
+    },
+             new Order() {
+    OrderId= 4,
+    Quality= 76,
+    UnitPrice = 23
+    },
+};
+
+EventDataBatch eventDataBatch = producerClient.CreateBatchAsync().GetAwaiter().GetResult();
+
+foreach (var order in listOfOrders)
+{
+    eventDataBatch.TryAdd(new Azure.Messaging.EventHubs.EventData(Encoding.UTF8.GetBytes(order.ToString())));
+}
+
+producerClient.SendAsync(eventDataBatch).GetAwaiter().GetResult();
+
+Console.WriteLine("Events Sent");
+
+Console.ReadKey();
